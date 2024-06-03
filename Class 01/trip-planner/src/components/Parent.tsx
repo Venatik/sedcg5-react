5546fd2f3f39eb3d58bf6b085cf44cb1a0f92ccd
Venatik <stefan.trajkovski008@gmail.com>
2024-06-03 20:39:19 +0200

    class01

--- Class 01/trip-planner/src/components/Parent.tsx
+++ Class 01/trip-planner/src/components/Parent.tsx
@@ -6,6 +6,9 @@ export const Parent = () => {
     <div>
       <h1>Hello from Parent</h1>
       <Child message="I am a prop passed from Parent." bucket={bucketList} />
+      {/* Instancing the Child component and passing props to it */}
+      {/* <Child message="I am a prop passed from Parent 2." bucket={bucketList} />
+      <Child message="I am a prop passed from Parent 3." bucket={bucketList} /> */}
     </div>
   );
 };
